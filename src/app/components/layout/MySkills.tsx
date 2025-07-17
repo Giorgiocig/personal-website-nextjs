056664e303a872a1cd2f277469a36442404c9a8f
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-17 16:52:36 +0200

    Rename Index with SkillsSection

--- src/app/components/layout/MySkills.tsx
+++ src/app/components/layout/MySkills.tsx
@@ -1,13 +1,9 @@
-import Index from "../common/Index";
+import { SkillsSectionMain } from "../common/SkillsSectionMain";
 
 export default function MySkills() {
   return (
-    <section
-      id="mySkills"
-      data-aos="fade-up"
-      className="container mx-auto pt-40 px-4"
-    >
-      <Index />
+    <section id="mySkills" data-aos="fade-up" className="pt-40 ">
+      <SkillsSectionMain />
     </section>
   );
 }
