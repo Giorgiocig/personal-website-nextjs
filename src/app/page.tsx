10c280be92248b27006dae5743d84403c0d028a4
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-17 16:54:14 +0200

    Embeed About  and ProjectSection to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,18 @@ import Hero from "./components/layout/Hero";
 import About from "./components/layout/About";
 import MySkills from "./components/layout/MySkills";
 import ExperienceSection from "./components/layout/ExperienceSection";
+import ProjectsSection from "./components/common/ProjectSection";
+import AboutSection from "./components/common/AboutSection";
 
 /**hydration error due to extensions no on the incognito */
 export default function Home() {
   return (
     <>
       <Hero />
-      <About />
+      <AboutSection />
       <ExperienceSection />
       <MySkills />
+      <ProjectsSection />
     </>
   );
 }
